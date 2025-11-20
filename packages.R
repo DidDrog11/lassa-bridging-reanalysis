9ad11e77b922d545ee5383b19fa6ffc2fc35ec61
David Simons <52014150+DidDrog11@users.noreply.github.com>
2025-11-20 12:22:46 +0100

    project initialisation

--- packages.R
+++ packages.R
@@ -1,19 +1,28 @@
 ## store your packages here
 
 ## core packages
-library(targets)
-library(tarchetypes)
 library(renv)
 library(dotenv)
 library(conflicted) # make sure we know where function calls are coming from and which is preferred. See .Rprofile file for default preferences
 
+## Project-Specific Packages
+library(spOccupancy) # JSDM and Spatial Modelling
+library(sf)
+library(terra) # Efficient raster handling
+
+# Data Wrangling and Visualisation
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# Epidemiology and Statistics
+library(MASS) # Used for quasi-binomial GLMs or other stats
+library(here) # File path management
 
 ## packages necessary for startup script
-library(here)
 library(usethis)
 
 ## recommended
 # library(fnmate) # quickly create functions
-# library(tflow) # allows for some nice targets add-ins
 
 
